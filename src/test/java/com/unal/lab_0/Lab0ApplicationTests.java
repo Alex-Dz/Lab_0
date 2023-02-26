ae7dfe3c79be166c80bfdd32e744f2ee36665440
Alex <alexander.cardenas@globant.com>
2023-02-26 11:32:30 -0500

    Initial commit + Data structure

---
+++ src/test/java/com/unal/lab_0/Lab0ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.unal.lab_0;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Lab0ApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
